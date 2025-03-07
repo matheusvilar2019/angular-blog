943d146af4a3d2c12fd47b287bb67cb6b9ec6364
matheusvilar2019 <43919709+matheusvilar2019@users.noreply.github.com>
2025-03-07 01:24:46 -0300

    components created with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    CardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
